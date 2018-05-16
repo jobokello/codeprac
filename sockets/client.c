35818f52aa8fb7491d3180c5943c37990bb93dee
novak-jack15 <jacknovak254@gmail.com>
2018-05-16 23:38:43 +0300

    zak

--- sockets/client.c
+++ sockets/client.c
@@ -1,4 +1,4 @@
-// Client side C/C++ program to demonstrate Socket programming
+// Client side C/C++ program to demonstrate Socket programming using TCP
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
