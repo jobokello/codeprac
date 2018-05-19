b3ace2a758706cadda718b2c1a6a7cb3bce9cb2a
Job Okello <jobokello5@gmail.com>
2018-05-19 11:59:12 +0300

    old code removed

--- C/circulantMatrix/client.c
+++ C/circulantMatrix/client.c
@@ -1,7 +1,9 @@
-//Name	 : Okello Job Opiyo
-//Reg no.: P15/37353/2016
+/****************** CLIENT CODE ****************/
 
-//client code
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
 #include<stdio.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
@@ -12,54 +14,47 @@
 #include<unistd.h>
 #include<arpa/inet.h>
 
-void main()
-{ 
-   struct sockaddr_in server;
-   int sock;
-   int matSize;
-   int myVector[1000];
-   int circulantMatrix[1000];
-   int i,j,k=1,l=1;
-   int retMatSize;
-   
-   server.sin_family = AF_INET;
-   server.sin_port = 2000;
-   server.sin_addr.s_addr = inet_addr("127.0.0.1");// server ip address
-   sock = socket(AF_INET,SOCK_STREAM, 0);
-
-   connect(sock, (struct sockaddr*) &server, sizeof(server));
-
-   printf("enter matrix size\n");
-   scanf("%d", &matSize);
-   printf("vector from clients is: [");
-
-   for(i=0; i<matSize; i++)
-   {
-
-      myVector[i] = k;
-      printf("%d ", myVector[i]);
-      k++;
-   }
-
-   printf("]\n");
-
-   send(sock, &matSize, sizeof(int) , 0);
-
-   send(sock, &myVector, matSize*sizeof(int) , 0);//sends string to the server
-
-   recv(sock, circulantMatrix, (matSize*matSize)*sizeof(int), 0);// receives reversed string from the server
-
-   retMatSize = matSize*matSize;
-
-   printf("the circulantMatrix is:\n");
-
-   for(i=0; i<retMatSize; i++)
-   {
-      printf("%d\t", circulantMatrix[i]);
-      if((l%matSize)==0)
-      {
-         printf("\n");
-      }
-      l++;
-   }
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8080
+  
+int main(int argc, char const *argv[])
+{
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+      
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    valread = read( sock , buffer, 1024);
+    printf("%s\n",buffer );
+    return 0;
 }
--- C/the awakening/hello world/main.c
+++
@@ -1,9 +0,0 @@
-/* this program displays hello world and the users name.
-compiled an written by okello job opiyo,reg no. p15/37353/2016*/
-#include <stdio.h>
-#include <stdlib.h>
-
-void main()
-{
-	printf("%X",467);
-}
