89d561f8aa99b07c1bceefb68d0183dd3ae563df
jobokello <jobokello5@gmail.com>
2018-07-20 01:00:42 +0300

    conflict fixed

--- python/ML-Drone/simpleGA.py
+++ python/ML-Drone/simpleGA.py
@@ -3,32 +3,20 @@ import string
 import random
 
 class Agent(object):
-<<<<<<< HEAD
 
-=======
-	"""docstring for Agent"""
->>>>>>> 20a73626473ef2b3ee9ffd4fca14558d7459c486
 	def __init__(self, length):
 
 		self.string = ''.join(random.choice(string.letters) for _ in range(length))
 		self.fitness = -1
 
 	def __str__(self):
-<<<<<<< HEAD
+		
 		return 'Generated String  ' + str(self.string) + ' Fitness to target string  = ' + str(self.fitness) + '%'
 
 in_str = None
 in_str_len = None
 population = 26
 generations = 10000
-=======
-		return 'String  ' + str(self.string) + ' fitness = ' + str(self.fitness)
-
-in_str = None
-in_str_len = None
-population = 20
-generations = 1000
->>>>>>> 20a73626473ef2b3ee9ffd4fca14558d7459c486
 
 def ga():
 	agents = init_agents(population, in_str_len)
@@ -52,17 +40,10 @@ def fitness(agents):
 	for agent in agents:
 		agent.fitness = fuzz.ratio(agent.string, in_str)
 
-<<<<<<< HEAD
 	return agents
 
 def selection(agents):
 	agents = sorted(agents, key=lambda agent: agent.fitness, reverse = True) 
-=======
-		return agents
-
-def selection(agents):
-	agent = sorted(agents, key = lambda agent: agent.fitness, reverse = True) 
->>>>>>> 20a73626473ef2b3ee9ffd4fca14558d7459c486
 	print '\n' .join(map(str, agents))
 	agents = agents[:int(0.2 + len(agents))]
 
@@ -77,18 +58,12 @@ def crossover(agents):
 
 		parent1 = random.choice(agents)
 		parent2 = random.choice(agents)
-<<<<<<< HEAD
 
-=======
->>>>>>> 20a73626473ef2b3ee9ffd4fca14558d7459c486
 		child1 = Agent(in_str_len)
 		child2 = Agent(in_str_len)
 
 		split = random.randomint(0, in_str_len)
-<<<<<<< HEAD
 
-=======
->>>>>>> 20a73626473ef2b3ee9ffd4fca14558d7459c486
 		child1.string = parent1.string[0,split] + parent2.string[split:in_str_len]
 		child2.string = parent2.string[0:split] + parent1.string[split:in_str_len]
 
@@ -112,11 +87,7 @@ def mutation(agents):
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
 	in_str = 'job'
-=======
-	in_str = 'abc'
->>>>>>> 20a73626473ef2b3ee9ffd4fca14558d7459c486
 	in_str_len = len(in_str)
 	ga()
 
