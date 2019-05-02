70f29f3ea05c8814083aa8ca588ec2b7acb52244
Job Okello <jobokello5@gmail.com>
2019-05-02 19:57:30 +0300

    momentum picked up

---
+++ java/ifelseif/src/ifelseif/decisions.java
@@ -0,0 +1,20 @@
+package ifelseif;
+
+public class decisions {
+	
+	public static void main(String args[]) {
+		
+		int boy = 18, girl = 17;
+		
+		if (boy < 18 || girl >= 67) {
+			System.out.print("We can allow such games!!");
+		}
+		
+		if (boy > 18 || girl < 67) {
+			System.out.print("Wacha Katambe!!");
+		}
+		
+				
+	}
+
+}
